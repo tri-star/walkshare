8ad8cdd0ad9e81554d044346df71dc85d426cd38
tristar <tristar.2nd@gmail.com>
2022-03-02 23:28:01 +0900

    iOS用のGoogleMap導入設定

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,13 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    if let path = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist") {
+      let nsDictionary = NSDictionary(contentsOfFile: path)
+
+      if let apiKey = nsDictionary?["API_KEY"] as? String {
+        GMSServices.provideAPIKey(apiKey)
+      }
+    }
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
