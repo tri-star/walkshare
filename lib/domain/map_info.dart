19981c07e1e1810a1fb92de370558b5fb6e9b34b
tristar <tristar.2nd@gmail.com>
2022-05-08 22:07:12 +0900

    不要なimportの削除

--- lib/domain/map_info.dart
+++ lib/domain/map_info.dart
@@ -1,6 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:strollog/domain/photo.dart';
 import 'package:strollog/domain/position.dart';
 import 'package:strollog/domain/user.dart';
