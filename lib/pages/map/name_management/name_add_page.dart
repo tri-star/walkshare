69bc9293b02e43a123e853a41c978fed2820839e
Tristar <tristar.2nd@gmail.com>
2022-06-24 05:16:48 +0900

    名前登録画面のデザイン調整

--- lib/pages/map/name_management/name_add_page.dart
+++ lib/pages/map/name_management/name_add_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:strollog/components/ws_button.dart';
+import 'package:strollog/components/ws_form_label.dart';
 import 'package:strollog/layouts/default_layout.dart';
 
 class NameAdd extends StatefulWidget {
@@ -12,42 +13,49 @@ class NameAdd extends StatefulWidget {
 class _NameAddState extends State<NameAdd> {
   @override
   Widget build(BuildContext context) {
-    return DefaultLayout(Container(
-        child: Container(
-            padding: const EdgeInsets.all(16),
-            child: Column(children: [
-              Row(children: const [
-                SizedBox(width: 100, child: Text('名前')),
-                Expanded(child: TextField()),
-              ]),
-              Row(children: const [
-                SizedBox(width: 100, child: Text('読み')),
-                Expanded(
-                    child: TextField(
-                  keyboardType: TextInputType.emailAddress,
-                )),
-              ]),
-              Row(children: const [
-                SizedBox(width: 100, child: Text('建物/場所')),
-                Expanded(child: TextField()),
-              ]),
-              Row(children: const [
-                SizedBox(width: 100, child: Text('説明')),
-                Expanded(child: TextField()),
-              ]),
-              Expanded(
-                  child: Row(mainAxisSize: MainAxisSize.max, children: [])),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  WSButton(
-                      title: '保存', icon: const Icon(Icons.save), onTap: null),
-                  WSButton(
-                      title: 'キャンセル',
-                      icon: const Icon(Icons.cancel),
-                      onTap: () => Navigator.of(context).pop()),
-                ],
-              )
-            ]))));
+    return DefaultLayout(SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Column(mainAxisSize: MainAxisSize.min, children: [
+          Column(crossAxisAlignment: CrossAxisAlignment.start, children: const [
+            WsFormLabel(
+              text: '名前',
+              required: true,
+            ),
+            TextField(),
+          ]),
+          Column(crossAxisAlignment: CrossAxisAlignment.start, children: const [
+            SizedBox(height: 20),
+            WsFormLabel(
+              text: '読み',
+              required: true,
+            ),
+            TextField(),
+          ]),
+          Column(crossAxisAlignment: CrossAxisAlignment.start, children: const [
+            SizedBox(height: 20),
+            WsFormLabel(w: 100, text: '建物/場所'),
+            TextField(),
+          ]),
+          Column(crossAxisAlignment: CrossAxisAlignment.start, children: const [
+            SizedBox(height: 20),
+            WsFormLabel(text: 'メモ'),
+            TextField(
+              minLines: 3,
+              maxLines: 10,
+              keyboardType: TextInputType.multiline,
+            ),
+          ]),
+          const SizedBox(height: 40),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              WSButton(title: '保存', icon: const Icon(Icons.save), onTap: null),
+              WSButton(
+                  title: 'キャンセル',
+                  icon: const Icon(Icons.cancel),
+                  onTap: () => Navigator.of(context).pop()),
+            ],
+          ),
+        ])));
   }
 }
