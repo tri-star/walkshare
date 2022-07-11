6400a1bc8067d40a35bd1f74b8eb9f53dfd0706f
Tristar <tristar.2nd@gmail.com>
2022-07-12 05:37:20 +0900

    写真プレビュー画面でも名前を表示するように対応

--- lib/pages/map/photo_preview_page.dart
+++ lib/pages/map/photo_preview_page.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:photo_view/photo_view.dart';
 import 'package:strollog/domain/map_info.dart';
 import 'package:strollog/domain/photo.dart';
@@ -38,71 +39,92 @@ class _PhotoPreviewPageState extends State<PhotoPreviewPage> {
         title: const Text('写真プレビュー'),
       ),
       body: Container(
-          child: Column(children: [
-        Expanded(
-            child: GestureDetector(
-                onHorizontalDragEnd: (details) => {
-                      if (details.primaryVelocity! > 0)
-                        {
-                          if (_index > 0) {setState(() => _index--)}
-                        }
-                      else if (details.primaryVelocity! < 0)
-                        {
-                          if (_index < _photoCount - 1)
-                            {setState(() => _index++)}
-                        }
-                    },
-                onVerticalDragEnd: (details) => {Navigator.of(context).pop()},
-                child: FutureBuilder<File>(
-                  future: _imageLoader.loadImageWithCache(
-                      widget.map, widget.photos[_index].getFileName()),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState != ConnectionState.done) {
-                      return const Center(child: CircularProgressIndicator());
-                    }
-                    if (snapshot.hasError) {
-                      return const Center(child: Text("画像のロードに失敗しました"));
-                    }
-                    if (!snapshot.hasData) {
-                      return const Center(child: Text("画像が見つかりません"));
-                    }
-                    return PhotoView(
-                      imageProvider: FileImage(snapshot.data!),
-                      minScale: PhotoViewComputedScale.contained * 0.8,
-                      initialScale: PhotoViewComputedScale.contained,
-                      basePosition: Alignment.center,
-                      backgroundDecoration:
-                          const BoxDecoration(color: Colors.black),
-                    );
+        child: FutureBuilder<DraftPhoto>(
+            future: _loadPhoto(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState != ConnectionState.done) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              if (snapshot.hasError) {
+                return const Center(child: Text("画像のロードに失敗しました"));
+              }
+              if (!snapshot.hasData) {
+                return const Center(child: Text("画像が見つかりません"));
+              }
+              var photo = snapshot.data!;
+              return Column(children: [
+                Expanded(
+                    child: GestureDetector(
+                  onHorizontalDragEnd: (details) => {
+                    if (details.primaryVelocity! > 0)
+                      {
+                        if (_index > 0) {setState(() => _index--)}
+                      }
+                    else if (details.primaryVelocity! < 0)
+                      {
+                        if (_index < _photoCount - 1) {setState(() => _index++)}
+                      }
                   },
-                ))),
-        // Text()
-        Row(children: [
-          Expanded(
-              child: TextButton.icon(
-                  onPressed: havePrev()
-                      ? () {
-                          setState(() {
-                            _index--;
-                          });
-                        }
-                      : null,
-                  icon: const Icon(Icons.arrow_back),
-                  label: const Text("前"))),
-          Expanded(
-              child: TextButton.icon(
-                  onPressed: haveNext()
-                      ? () {
-                          setState(() {
-                            _index++;
-                          });
-                        }
-                      : null,
-                  icon: const Icon(Icons.arrow_forward),
-                  label: const Text("次"))),
-        ]),
-        Row(children: const [SizedBox(height: 30, child: null)]),
-      ])),
+                  onVerticalDragEnd: (details) => {Navigator.of(context).pop()},
+                  child: PhotoView(
+                    imageProvider: FileImage(File(photo.imagePath)),
+                    minScale: PhotoViewComputedScale.contained * 0.8,
+                    initialScale: PhotoViewComputedScale.contained,
+                    basePosition: Alignment.center,
+                    backgroundDecoration:
+                        const BoxDecoration(color: Colors.black),
+                  ),
+                )),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  child: Row(children: [
+                    const SizedBox(
+                      width: 60,
+                      child: Text('名前'),
+                    ),
+                    Text(photo.name?.name ?? '名前なし'),
+                  ]),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  child: Row(children: [
+                    const SizedBox(
+                      width: 60,
+                      child: Text('登録日'),
+                    ),
+                    Text(photo?.savedPhoto?.date != null
+                        ? DateFormat('yyyy-MM-dd HH:mm')
+                            .format(photo.savedPhoto!.date!)
+                        : '-'),
+                  ]),
+                ),
+                Row(children: [
+                  Expanded(
+                      child: TextButton.icon(
+                          onPressed: havePrev()
+                              ? () {
+                                  setState(() {
+                                    _index--;
+                                  });
+                                }
+                              : null,
+                          icon: const Icon(Icons.arrow_back),
+                          label: const Text("前"))),
+                  Expanded(
+                      child: TextButton.icon(
+                          onPressed: haveNext()
+                              ? () {
+                                  setState(() {
+                                    _index++;
+                                  });
+                                }
+                              : null,
+                          icon: const Icon(Icons.arrow_forward),
+                          label: const Text("次"))),
+                ])
+              ]);
+            }),
+      ),
     );
   }
 
@@ -114,7 +136,10 @@ class _PhotoPreviewPageState extends State<PhotoPreviewPage> {
     return _index < (_photoCount - 1);
   }
 
-  // String getPhotoDate(Photo photo, int index) {
-  //   return photo.
-  // }
+  Future<DraftPhoto> _loadPhoto() async {
+    var cache = await _imageLoader.loadImageWithCache(
+        widget.map, widget.photos[_index].getFileName());
+
+    return DraftPhoto.saved(widget.photos[_index], cachePath: cache.path);
+  }
 }
