a37487ba16b325210528141388efe62c19c6c1bd
Tristar <tristar.2nd@gmail.com>
2022-08-15 01:28:31 +0900

    faker関連のクラスを移動

--- lib/pages/name_management/test/name_edit_page_store_test.dart
+++ lib/pages/name_management/test/name_edit_page_store_test.dart
@@ -4,8 +4,8 @@ import 'package:strollog/domain/map_info.dart';
 import 'package:strollog/domain/name.dart';
 import 'package:strollog/domain/user.dart';
 import 'package:strollog/lib/test/faker_builder.dart';
-import 'package:strollog/lib/test/fakers/map_info_faker.dart';
-import 'package:strollog/lib/test/fakers/name_faker.dart';
+import 'package:strollog/fakers/map_info_faker.dart';
+import 'package:strollog/fakers/name_faker.dart';
 import 'package:strollog/lib/test/firebase_test_utils.dart';
 import 'package:strollog/lib/test/image_cropper_stub.dart';
 import 'package:strollog/lib/test/image_picker_stub.dart';
@@ -66,7 +66,6 @@ void main() {
         // 顔写真なしの名前データを作成し、保存しておく
         var mapInfo = FakerBuilder<MapInfo>().create(MapInfoFaker.prepare());
         var name = FakerBuilder<Name>().create(NameFaker.prepare());
-
         await mapInfoRepository.save(mapInfo);
         await nameRepository.save(testUser, mapInfo.id!, name);
 
@@ -85,7 +84,24 @@ void main() {
     });
 
     group('事前に写真登録ありの場合', () {
-      test('画像なしで保存__元の画像が残ること', () async {});
+      test('画像なしで保存__元の画像が残ること', () async {
+        // 顔写真なしの名前データを作成し、保存しておく
+        var mapInfo = FakerBuilder<MapInfo>().create(MapInfoFaker.prepare());
+        var name = FakerBuilder<Name>().create(NameFaker.prepare());
+        await mapInfoRepository.save(mapInfo);
+        await nameRepository.save(testUser, mapInfo.id!, name);
+
+        await store.initialize(mapInfo.id!, name.id);
+        // 名前等を変更して保存を実行
+        var expectedName = '名前変更';
+        store.name.name = expectedName;
+        store.setInteracted(true);
+
+        await store.save();
+
+        var result = await nameRepository.fetchNameById(mapInfo.id!, name.id);
+        expect(result!.name, expectedName);
+      });
       test('画像を付けて保存__新しい写真が保存されること', () async {});
     });
 
