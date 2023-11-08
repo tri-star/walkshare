e637825ff9a67cd7dbde3eb8a1be0f2d1b7fc990
tristar <tristar.2nd@gmail.com>
2023-11-08 23:43:20 +0900

    画像のサムネイルを返す関数の追加

--- functions/src/image-cache.ts
+++ functions/src/image-cache.ts
@@ -1,67 +1,91 @@
-import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
-import * as sharp from 'sharp';
-import { tmpdir } from 'os';
-import { join, dirname } from 'path';
-import * as fs from 'fs-extra';
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import * as sharp from "sharp";
+import {tmpdir} from "os";
+import {join, dirname, basename} from "path";
+import * as fs from "fs-extra";
 
-admin.initializeApp();
+export const generateThumbnail = functions.runWith({memory: "512MB"}).https.onRequest(async (req, res) => {
+  // リクエストヘッダの認証
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
+  const token = authHeader.split("Bearer ")[1];
+  try {
+    await admin.auth().verifyIdToken(token);
+  } catch (e) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
 
-exports.generateThumbnail = functions.https.onRequest(async (req, res) => {
+  const filePath = req.query.key as string | undefined;
+  const widthString = req.query.w as string | undefined;
+  const heightString = req.query.h as string | undefined;
 
-    // TODO: リクエストヘッダの認証
+  if (filePath === undefined) {
+    res.status(400).send("No file path defined");
+    return;
+  }
+  if (widthString === undefined && heightString === undefined) {
+    res.status(400).send("w,h or both must be defined");
+    return;
+  }
 
-    const filePath = req.query.key as string | undefined;
-    const widthString = req.query.w as string | undefined;
-    const heightString = req.query.h as string | undefined;
+  const w = widthString !== undefined ? Number(widthString) : undefined;
+  const h = heightString !== undefined ? Number(heightString) : undefined;
+  const fileName = basename(filePath);
+  const fileDir = dirname(filePath);
+  const cachePath = join(fileDir, makeCacheFileName(fileName, w, h));
+  const expirationDate = new Date();
+  expirationDate.setDate(expirationDate.getDate() + 7);
 
-    if (filePath === undefined) {
-        res.status(400).send('No file path defined');
-        return;
-    }
-    if (widthString === undefined && heightString === undefined) {
-        res.status(400).send('w,h or both must be defined');
-        return;
-    }
+  const bucket = admin.storage().bucket();
+  const workDir = tmpdir();
+  const originalFilePath = join(workDir, fileName);
+  const resizedFilePath = join(workDir, `thumb_${fileName}`);
 
-    const w = widthString !== undefined ? Number(widthString) : undefined;
-    const h = heightString !== undefined ? Number(heightString) : undefined;
-    const cachePath = makeCacheFileName(filePath, w, h);
+  // キャッシュが存在する場合はそのまま返す
+  const cacheFile = bucket.file(cachePath);
+  const exists = await cacheFile.exists();
+  if (exists[0]) {
+    const url = await bucket.file(cachePath).getSignedUrl({action: "read", expires: expirationDate});
+    res.redirect(302, url[0]);
+    return;
+  }
 
-    const bucket = admin.storage().bucket();
-    const tempLocalFile = join(tmpdir(), filePath);
-    const tempLocalDir = dirname(tempLocalFile);
+  // 元画像のサムネイルを作成
+  await fs.ensureDir(workDir);
+  await bucket.file(filePath).download({destination: originalFilePath});
 
-    // キャッシュが存在する場合はそのまま返す
-    const cacheFile = bucket.file(cachePath);
-    const exists = await cacheFile.exists();
-    if (exists[0]) {
-        res.sendFile(cachePath);
-        return;
-    }
+  const thumbnail = sharp(originalFilePath)
+    .sharpen()
+    .resize(w, h, {
+      kernel: sharp.kernel.lanczos3,
+    });
 
-    // 元画像のサムネイルを作成
-    await fs.ensureDir(tempLocalDir);
-    await bucket.file(filePath).download({ destination: tempLocalFile });
+  if (originalFilePath.split(".").pop() === "jpg") {
+    thumbnail.jpeg({quality: 90});
+  }
 
-    await sharp(tempLocalFile)
-        .resize(w, h)
-        .toFile(tempLocalFile);
+  await thumbnail.toFile(resizedFilePath);
 
-    await bucket.upload(tempLocalFile, { destination: cachePath });
+  await bucket.upload(resizedFilePath, {destination: cachePath});
 
-    // ファイルをローカル上からunlinkした後、レスポンスを返す
-    fs.unlinkSync(tempLocalFile);
+  const url = await bucket.file(cachePath).getSignedUrl({action: "read", expires: expirationDate});
+  res.redirect(302, url[0]);
 
-    res.sendFile(tempLocalFile);
+  // ファイルをローカル上からunlinkした後、レスポンスを返す
+  fs.unlinkSync(originalFilePath);
+  fs.unlinkSync(resizedFilePath);
 });
 
 
 function makeCacheFileName(key: string, w: number | undefined, h: number | undefined) {
-    const sizePart = []
-    const extension = key.split('.').pop()
-    sizePart.push(w?.toString() ?? 'auto')
-    sizePart.push(h?.toString() ?? 'auto')
-    return `/_cache/${key}_${sizePart.join(':')}.${extension}`
-
+  const sizePart = [];
+  const extension = key.split(".").pop();
+  sizePart.push(w?.toString() ?? "auto");
+  sizePart.push(h?.toString() ?? "auto");
+  return `/_cache/${key}_${sizePart.join(":")}.${extension}`;
 }
